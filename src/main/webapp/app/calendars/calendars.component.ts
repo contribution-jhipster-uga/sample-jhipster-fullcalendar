f380b49c5494da30685f20ed5ec230beb8e15d77
Ergi SALA <ergisala@live.com>
2020-02-20 11:27:08 +0100

    Event display ok

--- src/main/webapp/app/calendars/calendars.component.ts
+++ src/main/webapp/app/calendars/calendars.component.ts
@@ -8,6 +8,8 @@ import dayGridPlugin from '@fullcalendar/daygrid';
 
 import { CalendarService } from 'app/entities/calendar/calendar.service';
 import { ICalendar } from 'app/shared/model/calendar.model';
+import { ICalendarEvent } from 'app/shared/model/calendar-event.model';
+import { CalendarEventService } from 'app/entities/calendar-event/calendar-event.service';
 
 @Component({
   selector: 'jhi-calendars',
@@ -15,11 +17,15 @@ import { ICalendar } from 'app/shared/model/calendar.model';
 })
 export class CalendarsComponent implements OnInit, OnDestroy {
   calendarList?: ICalendar[];
+  calendarEvents?: ICalendarEvent[];
+  displayedEvents?: {}[];
+  checkedCals?: { calid?: number, checked: boolean }[];
   eventSubscriber?: Subscription;
   calendarPlugins = [dayGridPlugin];
 
   constructor(
     protected calendarService: CalendarService,
+    protected calendarEventService: CalendarEventService,
     protected activatedRoute: ActivatedRoute,
     protected router: Router,
     protected eventManager: JhiEventManager,
@@ -28,15 +34,16 @@ export class CalendarsComponent implements OnInit, OnDestroy {
   loadAll(): void {
     this.calendarService
       .query()
-      .subscribe(
-        (res: HttpResponse<ICalendar[]>) => this.onSuccess(res.body),
-        () => this.onError()
-      );
+      .subscribe((res: HttpResponse<ICalendar[]>) => this.onCalendarSuccess(res.body));
+    this.calendarEventService
+      .query()
+      .subscribe((res: HttpResponse<ICalendarEvent[]>) => this.onCalendarEventSuccess(res.body));
   }
 
   ngOnInit(): void {
     this.loadAll()
     this.registerChangeInCalendars();
+    this.registerChangeInCalendarEvents();
   }
 
   ngOnDestroy(): void {
@@ -49,11 +56,38 @@ export class CalendarsComponent implements OnInit, OnDestroy {
     this.eventSubscriber = this.eventManager.subscribe('calendarsModification', () => this.loadAll());
   }
 
-  protected onSuccess(data: ICalendar[] | null): void {
-    this.calendarList = data || [];
+  registerChangeInCalendarEvents(): void {
+    this.eventSubscriber = this.eventManager.subscribe('calendarEventListModification', () => this.loadAll());
   }
 
-  protected onError(): void {
+  protected onCalendarSuccess(data: ICalendar[] | null): void {
+    this.calendarList = data || [];
+    this.checkedCals = [];
+    this.calendarList.forEach(c => {
+      this.checkedCals!.push({ calid: c.id, checked: true });
+    });
+  }
+
+  protected onCalendarEventSuccess(data: ICalendarEvent[] | null): void {
+    this.calendarEvents = data || [];
+    this.displayedEvents = [];
+    this.calendarEvents.forEach(e => {
+      if (e.calendarId)
+        this.displayedEvents!.push({ title: e.title, start: e.startDate!.toISOString(), end: e.endDate!.toISOString() });
+    });
+  }
+
+  protected eventsToDisplay(cid: number, event: any): void {
+    this.displayedEvents = [];
+    (this.checkedCals!.find(c => c.calid === cid))!.checked = event.target.checked;
+    this.checkedCals!.forEach(c => {
+      if (c.checked) {
+        const e = this.calendarEvents!.filter(f => f.calendarId === c.calid);
+        e.forEach(ev => {
+          this.displayedEvents!.push({ title: ev.title, start: ev.startDate!.toISOString(), end: ev.endDate!.toISOString() });
+        });
+      }
+    });
   }
 }
 
