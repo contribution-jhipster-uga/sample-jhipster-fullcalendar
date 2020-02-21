ad5f07413691f8f85e9d492d421cdb939a4eb7c7
Ergi SALA <ergisala@live.com>
2020-02-21 11:07:09 +0100

    Event display ok

--- src/main/java/com/mycompany/myapp/config/SecurityConfiguration.java
+++ src/main/java/com/mycompany/myapp/config/SecurityConfiguration.java
@@ -77,7 +77,6 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
             .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
         .and()
             .authorizeRequests()
-            .antMatchers("/api/calendars").permitAll()
             .antMatchers("/api/authenticate").permitAll()
             .antMatchers("/api/register").permitAll()
             .antMatchers("/api/activate").permitAll()
