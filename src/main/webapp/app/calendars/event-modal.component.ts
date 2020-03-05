3c1dccd2ed630d44e396a512e21be51f31e18510
Corentin Chasseguet <chasseguetcorentin@yahoo.fr>
2020-03-05 09:11:40 +0100

    Modal completed

--- src/main/webapp/app/calendars/event-modal.component.ts
+++ src/main/webapp/app/calendars/event-modal.component.ts
@@ -2,20 +2,12 @@ import { Component, Input } from '@angular/core';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 import { JhiAlertService } from 'ng-jhipster';
 
-
 @Component({
   selector: 'jhi-event-modal',
   templateUrl: './event-modal.component.html'
 })
 export class EventModalComponent {
   @Input() eventObject: any;
-  
-  constructor(
-    public activeModal: NgbActiveModal,
-    protected jhiAlertService: JhiAlertService,
-  ) {}
 
-  toto(): void {
-    // console.log(this.eventObject);
-  }
+  constructor(public activeModal: NgbActiveModal, protected jhiAlertService: JhiAlertService) {}
 }
